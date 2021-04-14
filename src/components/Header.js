add16249248a0ed554abec8d242d98672251578f
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 09:25:55 +0100

    Create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Header = () => {
-    return (
-        <div>
-            Trackify
-        </div>
-    )
-}
+const Header = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
 
-export default Header
+Header.defaultProps = {
+  title: "Trackify",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS IN JS
+const headingStyle = {
+  color: "red",
+  backgroundColor: "black",
+};
+
+export default Header;
