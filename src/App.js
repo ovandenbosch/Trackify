2a43e0dd6bc1ac35c059ed83a1b4ec98e2bb0aa4
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 15:27:16 +0100

    Task Submit - add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,13 @@ function App() {
     },
   ]);
 
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -41,6 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
   const [text, setText] = useState("");
   const [day, setDay] = useState("");
   const [reminder, setReminder] = useState(false);
 
+  const onSumbit = (e) => {
+    e.preventDefault();
+
+    if(!text) {
+        alert('Please add a task')
+        return
+    }
+
+    onAdd({ text, day, reminder })
+
+    setText('')
+    setDay('')
+    setReminder(false)
+  };
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSumbit}>
       <div className="form-control">
         <label>Task</label>
         <input
@@ -29,6 +44,7 @@ const AddTask = () => {
         <label>Set Reminder</label>
         <input
           type="checkbox"
+          checked={reminder}
           value={reminder}
           onChange={(e) => setReminder(e.currentTarget.checked)}
         />
