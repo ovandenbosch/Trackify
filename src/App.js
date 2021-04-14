6ccab2438c30d0902b7f83f8292775ba05cf8aed
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 10:27:12 +0100

    Update classnames

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import Header from './components/Header'
-
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting At School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 6th at 2:30pm",
+      reminder: false,
+    },
+  ]);
   return (
-    <div className="App">
+    <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
