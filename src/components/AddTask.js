32c039db6d0893894772c5a1650d974294456157
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 13:19:57 +0100

    Create add task form

---
+++ src/components/AddTask.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AddTask = () => {
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <input type="text" placeholder="Add Task" />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input type="text" placeholder="Add Day & Time" />
+      </div>
+      <div className="form-control">
+        <label>Set Reminder</label>
+        <input type="checkbox" />
+      </div>
+
+      <input type="submit" value="Save Task" />
+    </form>
+  );
+};
+
+export default AddTask;
