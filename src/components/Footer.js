ca426125c8709f2f7997bba0f2493a050e525a91
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 17:37:51 +0100

    Create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2021</p>
+      <p><a href="https://ovdb.tech"> Oliver van den Bosch</a></p>
+      <a href="/about">About</a>
+    </footer>
+  );
+};
+
+export default Footer;
