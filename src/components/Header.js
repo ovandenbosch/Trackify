3b887aa082280ed0a45a586fe40ff702bae50011
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 18:00:32 +0100

    Use location from rrdom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
+      {location.pathname === '/' && <Button
         color={showAdd ? "red" : "black"}
         text={showAdd ? "Close" : "Add"}
         onClick={onAdd}
-      />
+      />}
     </header>
   );
 };
