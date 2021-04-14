1ae396ec346f3b9fab07771b0f50fbe79c3c4b86
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 13:15:56 +0100

    Reminder - onToggle function

--- src/App.js
+++ src/App.js
@@ -29,11 +29,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No tasks to show, add one to get started..."
       )}
