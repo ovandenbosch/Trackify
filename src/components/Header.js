694a401185d44a5f09f68132beacee288e82ab8f
Oliver van den Bosch <vanders06@icloud.com>
2021-04-14 09:45:01 +0100

    Add text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
-    <div>
+    <header className='header'>
       <h1>{title}</h1>
-    </div>
+      <Button color="green" text="Add" onClick={onClick}/>
+    </header>
   );
 };
 
